0b67b518f42a997381acddffe0d1f0544df94ee2
yshir <yshir0020@gmail.com>
2023-03-12 14:28:55 +0900

    feat: setup supabase client

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    SUPABASE_PROJECT_ID: process.env.SUPABASE_PROJECT_ID,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
